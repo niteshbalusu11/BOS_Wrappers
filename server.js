fc3dc3f62739f42f419d4a5c43399f526d90bd40
Nitesh <niteshbalusu@icloud.com>
2021-07-19 16:37:58 -0400

    changing port

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/getChainFees', async (req, res, next) => {
 
   })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 
 app.listen(port, () => { console.log(`listening on ${port}`) })
